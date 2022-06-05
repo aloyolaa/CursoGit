e395dc0bf3d02fa6bab2834b98c721bb38d0f5bd
Alan Loyola <alanraulleonelloyolaalvarez@gmail.com>
2022-06-05 18:33:18 -0500

    Se creo un nuevo objeto Persona

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from Persona import Persona
 
 persona = Persona("Alan", "Loyola")
 
+nombre1 = input("Ingrese su nombre: ")
+apellido1 = input("Ingrese su apellido: ")
+persona1 = Persona(nombre1, apellido1)
+
+print("Detalle de Personas:")
 print(persona.verDetalle())
+print(persona1.verDetalle())
