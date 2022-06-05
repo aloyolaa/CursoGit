13b8d0ff8d829760ec11c4c1ca84220a869e5999
Alan Loyola <alanraulleonelloyolaalvarez@gmail.com>
2022-06-05 18:35:33 -0500

    Se imprimen los objetos persona linea por linea

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ nombre1 = input("Ingrese su nombre: ")
 apellido1 = input("Ingrese su apellido: ")
 persona1 = Persona(nombre1, apellido1)
 
-print("Detalle de Personas:")
+print("Detalle de Personas linea por linea:")
 print(persona.verDetalle())
 print(persona1.verDetalle())
